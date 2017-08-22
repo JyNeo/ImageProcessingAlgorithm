558675447afd195d091c651b59945a9727603161
JyNeo <1322893818@qq.com>
2017-08-22 09:42:08 +0800

    updated LogTransformations.cpp

--- LogTransformations/LogTransformations.cpp
+++ LogTransformations/LogTransformations.cpp
@@ -10,7 +10,7 @@
 
 using namespace cv;
 
-//使用对数变换来扩展图像中的暗像素的值，同时压缩更高灰度级的值
+//使用对数变换来扩展图像中的暗像素的值，同时压缩更高灰度级的值。s = c * log(1 + r); (c = 1)
 int main(int argc, char *argv[])
 {
 	Mat image = imread("F:\\DIP3E_Original_Images\\DIP3E_Original_Images_CH03\\Fig0305(a)(DFT_no_log).tif");
@@ -36,5 +36,53 @@ int main(int argc, char *argv[])
 	imshow("DstImage", imageLog);
 
 	waitKey();
+	return 0;
+}
+
+/*********************************************************************************************************************************/
+
+#include <highgui/highgui.hpp>
+#include <core/core.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[]) 
+{
+	IplImage* image = cvLoadImage("F:\\DIP3E_Original_Images\\DIP3E_Original_Images_CH03\\Fig0305(a)(DFT_no_log).tif");
+	if (image == NULL)
+	{
+		return 0;
+	}
+
+	cvNamedWindow("SrcImage");
+	cvShowImage("SrcImage", image);
+
+	int width = image->width;
+	int height = image->height;
+	int channels = image->nChannels;
+	int step = image->widthStep;
+	uchar *data = (uchar *)image->imageData;
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			for (int k = 0; k < channels; k++)
+			{
+				data[i*step + j*channels + k] = log(1 + data[i*step + j*channels + k]);
+			}
+		}
+	}
+
+	// 归一化到 0 ~ 255
+	cvNormalize(image, image, 0, 255, CV_MINMAX);
+
+	cvNamedWindow("DstImage");
+	cvShowImage("DstImage", image);
+
+	cvWaitKey();
+	cvReleaseImage(&image);
+	cvDestroyWindow("SrcImage");
+	cvDestroyWindow("DstImage");
 	return 0;
 }
